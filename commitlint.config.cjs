ab206d5a2b8da7c73a2f5c9554e158f3da5a7803
Sherartsu <jose.merino9108@gmail.com>
2026-04-01 16:07:32 -0600

    fix: commitlint cleaned

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,22 +1,25 @@
-// commitlint.config.cjs  👈 renómbralo a .cjs
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [2, 'always', [
-      'feat',
-      'fix',
-      'ui',
-      'refactor',
-      'chore',
-      'docs',
-      'test',
-      'perf',
-      'revert',
+      'feat', 'fix', 'ui', 'refactor',
+      'chore', 'docs', 'test', 'perf', 'revert',
     ]],
     'type-case': [2, 'always', 'lower-case'],
     'subject-empty': [2, 'never'],
     'subject-case': [2, 'always', 'lower-case'],
     'subject-max-length': [2, 'always', 72],
     'subject-min-length': [2, 'always', 10],
-  }
+  },
+  helpUrl: false,  // quita el link de ayuda
+  prompt: {
+    messages: {
+      skip: '(presiona enter para omitir)',
+      max: 'máximo %d caracteres',
+      min: 'mínimo %d caracteres',
+      emptyWarning: 'el campo no puede estar vacío',
+      upperLimitWarning: 'demasiado largo',
+      lowerLimitWarning: 'demasiado corto',
+    },
+  },
 }
