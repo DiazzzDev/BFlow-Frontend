e37d7ed634ac637688fc29477ed212c3f053fd4d
Díaz <ed.diaz.hz@gmail.com>
2026-06-06 22:26:30 -0600

    fix: vercel config for internal navegation

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,3 +1,1 @@
-export const config = { 
-    API_BASE_URL: import.meta.env.MODE === 'development' ? '' : import.meta.env.VITE_API_URL 
-};
+export const config = { API_BASE_URL: import.meta.env.VITE_API_URL };
